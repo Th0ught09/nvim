b576d429463329952c1b39baba4675d62fcb932d
Kirk Matt <kirkmatt@proton.me>
2025-08-21 21:49:20 +0100

    initial commit

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,5 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  version = "1.32.0",
+  dependencies = { "williamboman/mason.nvim" },
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  version = "1.11.0",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+    },
+  },
+}
