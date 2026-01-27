2f6fc6b6e908186a8b1b92a080270a2a5d369605
Kirk Matt <kirkmatt@proton.me>
2026-01-27 13:26:24 +0000

    added org-roam

---
+++ lua/plugins/org-roam.lua
@@ -0,0 +1,20 @@
+return {
+  "chipsenkbeil/org-roam.nvim",
+  tag = "0.2.0",
+  dependencies = {
+    {
+      "nvim-orgmode/orgmode",
+      tag = "0.7.0",
+    },
+  },
+  config = function()
+    require("org-roam").setup({
+      directory = "~/org_roam_files",
+      -- optional
+      org_files = {
+        "~/org/*.org",
+        "~/org/**/*.org",
+      },
+    })
+  end,
+}
