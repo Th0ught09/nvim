b185a02ae3316a009829c8cd292489d6087581ce
Kirk Matt <kirkmatt@proton.me>
2026-02-27 10:25:17 +0000

    disabled blink and added vimtex

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
-  -- { "saadparwiaz1/cmp_luasnip", enabled = false },
-  -- { "Saghen/blink.cmp", enabled = false },
+  { "saadparwiaz1/cmp_luasnip", enabled = false },
+  { "Saghen/blink.cmp", enabled = false },
   -- { "mrcjkb/rustaceanvim", enabled = false },
   { "chipsenkbeil/org-roam.nvim", enabled = false },
   { "simrat39/rust-tools.nvim", enabled = false },
