f431a316862b70643015d02928e002586a69b59d
Kirk Matt <kirkmatt@proton.me>
2026-06-09 20:38:34 +0100

    no auto check for updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
   },
   install = { colorscheme = { "tokyonight", "gruvbox" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
